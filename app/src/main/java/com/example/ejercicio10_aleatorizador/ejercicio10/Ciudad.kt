9b94abec8267e346ad8b06a052d208edd49402c1
Javier Alcantarilla Ballesteros <javalc310@alu.centroafuera.es>
2023-01-31 12:56:57 +0100

    aaaaaaaa

--- app/src/main/java/com/example/ejercicio10_aleatorizador/ejercicio10/Ciudad.kt
+++ app/src/main/java/com/example/ejercicio10_aleatorizador/ejercicio10/Ciudad.kt
@@ -3,10 +3,19 @@ package com.example.ejercicio10_aleatorizador.ejercicio10
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ejercicio10_aleatorizador.R
+import com.example.ejercicio10_aleatorizador.databinding.ActivityCiudadBinding
 
 class Ciudad : AppCompatActivity() {
+    lateinit var binding: ActivityCiudadBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ciudad)
+        binding= ActivityCiudadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        botones()
+    }
+
+    private fun botones(){
+
     }
 }
--- app/src/main/java/com/example/ejercicio10_aleatorizador/ejercicio10/Objeto.kt
+++ app/src/main/java/com/example/ejercicio10_aleatorizador/ejercicio10/Objeto.kt
@@ -1,5 +1,6 @@
 package com.example.ejercicio10_aleatorizador.ejercicio10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ejercicio10_aleatorizador.R
@@ -12,6 +13,18 @@ class Objeto : AppCompatActivity() {
         binding= ActivityObjetoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        botones()
+    }
 
+    private fun botones(){
+        binding.recoger.setOnClickListener {
+            val intent = Intent(this, SiguienteEj::class.java)
+            startActivity(intent)
+        }
+
+        binding.continuar1.setOnClickListener {
+            val intent = Intent(this, Dado::class.java)
+            startActivity(intent)
+        }
     }
 }
